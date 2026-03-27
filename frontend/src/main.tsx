f227bd3bc8bdb18fe0bfda5a4bca95dc3562f1fd
ol1oly <olivier.girardville@outlook.com>
2026-03-27 18:23:35 -0400

    feat: mount OverlayCanvas in transparent full-screen root

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { OverlayCanvas } from './components/OverlayCanvas'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className="w-screen h-screen bg-transparent overflow-hidden relative">
+      <OverlayCanvas />
+    </div>
   </StrictMode>,
 )
